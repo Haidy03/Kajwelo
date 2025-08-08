c8887696dade9a51012b17c0115891d1d069f6cc
Haidy Mohamed <haidymohamed650@gmail.com>
2025-08-08 16:15:54 +0300

    login page + navbar + signup

---
+++ JS/login.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",function(){
+    const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
+}); // end of window load
