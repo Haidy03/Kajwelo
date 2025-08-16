d8fbd24dd8bd23a6217df8b00e0ae4431eead380
Haidy Mohamed <haidymohamed650@gmail.com>
2025-08-16 16:01:51 +0300

    ...............

---
+++ JS/sellerNavigation.js
@@ -0,0 +1,29 @@
+window.addEventListener("load",function()
+{
+    const addProductBtn=document.getElementById("addproductbtn");
+    const AddProdBtn=document.getElementById("AddProdBtn");
+    const DashboardBtn=document.getElementById("DashBtn");
+    const InventoryBtn=document.getElementById("InventoryBtn");
+
+    
+
+    // addProductBtn.addEventListener("click",function()
+    // {
+    //     window.location.href="SelleraAddProduct.html";
+    // });
+    AddProdBtn.addEventListener("click",function()
+    {
+        window.location.href="SelleraAddProduct.html";
+    });
+    DashboardBtn.addEventListener("click",function()
+    {
+        window.location.href="../Pages/sellerdashboard.html";
+    });
+    InventoryBtn.addEventListener("click",function()
+    {
+        window.location.href="../Pages/SellerInventory.html";
+    });
+
+   
+
+});
